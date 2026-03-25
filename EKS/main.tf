e1b398e16e88d9de64d61f1f7dbafc77f23e6948
Raja <ciscoonlinetest@gmail.com>
2026-03-26 01:55:57 +0530

    Changed instance type from t2.small to t3.medium

--- EKS/main.tf
+++ EKS/main.tf
@@ -50,7 +50,7 @@ module "eks" {
     nodes = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["t2.small"]
+      instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 3
